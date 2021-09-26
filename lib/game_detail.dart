cb9e341df262f6925b03c7baca2be9c05edfad0b
DBedis <dbedis@ymail.com>
2021-09-26 23:38:35 +0100

    details+inscription

---
+++ lib/game_detail.dart
@@ -0,0 +1,15 @@
+import 'package:workshop2/product_info.dart';
+import 'package:flutter/material.dart';
+
+class Game extends StatelessWidget {
+  const Game({Key? key}) : super(key: key);
+final ProductInfo product;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+      ),
+    );
+  }
+}
